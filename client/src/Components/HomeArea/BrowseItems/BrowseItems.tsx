1759464dd50fe7de60350a1dcd04531c2f8a3f92
Trafalguy <122468039+Trafalguy@users.noreply.github.com>
2023-03-27 16:25:39 +0300

    Add files via upload

---
+++ client/src/Components/HomeArea/BrowseItems/BrowseItems.tsx
@@ -0,0 +1,10 @@
+import './BrowseItems.css'
+
+
+export default function BrowseItems(): JSX.Element {
+    return (
+        <div className='BrowseItems'>
+            <h1>Browse items works !</h1>
+        </div>
+    )
+}
