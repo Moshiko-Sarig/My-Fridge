22f500f01e15c631717315a9d580293a9a77a5ed
Trafalguy <122468039+Trafalguy@users.noreply.github.com>
2023-03-28 23:19:57 +0300

    Delete client directory

--- client/src/Components/Redux/Reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import userLoginReducer from "./userLoginReducer";
-
-const allReducers = combineReducers({
-    isLogged: userLoginReducer
-});
-
-
-export default allReducers;
